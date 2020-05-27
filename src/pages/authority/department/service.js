40f508002b93cd5ef203cb7f22bb83ecb8cc0df2
wuhanchu <whcwuhanchu@gmail.com>
2020-05-27 11:57:58 +0800

    delete

--- src/pages/authority/department/service.js
+++
@@ -1,8 +0,0 @@
-import schemas from "./schema"
-
-import { createApi } from "@/outter/fr-schema/src/service";
-
-// 用户
-let service = createApi("user_auth/department", schemas, null, "eq.")
-
-export default service
