a55c7a095a624c019b240cf94fe5e5ca060168ea
wuhanchu <whcwuhanchu@gmail.com>
2020-05-28 19:03:08 +0800

    delete unused schema

--- src/schemas/check/service.js
+++
@@ -1,5 +0,0 @@
-import { createApi } from "@/outter/fr-schema/src/service"
-
-const service = createApi("contract", null, { skipOauth: true }, "eq.")
-service.getCompareList = createApi("contract_compare", null, { skipOauth: true },).get
-export default service
--- src/schemas/contract/service.js
+++
@@ -1,7 +0,0 @@
-import { createApi } from "@/outter/fr-schema/src/service"
-import { schemaFieldType } from "@/outter/fr-schema/src/schema"
-
-const service = createApi("contract_compare", null, { skipOauth: true }, "eq.")
-// service.search = createApi("rpc/question_search", schema).getBasic
-
-export default service
